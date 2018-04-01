bcb9191f0e4827c103e1083c2b1a287bb140690a
LongDo <long.do@unicorn.vn>
2018-04-01 23:25:59 +0700

    Con Cac

--- Assets/Scripts/FullscreenSprider.cs
+++ Assets/Scripts/FullscreenSprider.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class FullscreenSprider : MonoBehaviour {
 
-       
+
+    // Use this for initialization
     void Start()
     {
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        Vector3 tempScale = transform.localScale;
+        float TARGET_WIDTH = 640.0f;
+        float TARGET_HEIGHT = 480.0f;
+        int PIXELS_TO_UNITS = 30; // 1:1 ratio of pixels to units
 
-        float height = sr.bounds.size.y;
-        float width = sr.bounds.size.x;
+        float desiredRatio = TARGET_WIDTH / TARGET_HEIGHT;
+        float currentRatio = (float)Screen.width / (float)Screen.height;
 
-        float worlHeight = Camera.main.orthographicSize * 2f;
-        float worlWidth = worlHeight * Screen.width / Screen.height;
-
-        tempScale.y = worlHeight / height;
-        tempScale.x = worlWidth / width;
-
-        transform.localScale = tempScale;
+        if (currentRatio >= desiredRatio)
+        {
+            // Our resolution has plenty of width, so we just need to use the height to determine the camera size
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS;
+        }
+        else
+        {
+            // Our camera needs to zoom out further than just fitting in the height of the image.
+            // Determine how much bigger it needs to be, then apply that to our original algorithm.
+            float differenceInSize = desiredRatio / currentRatio;
+            Camera.main.orthographicSize = TARGET_HEIGHT / 4 / PIXELS_TO_UNITS * differenceInSize;
+        }
     }
 }
